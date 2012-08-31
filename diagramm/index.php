30674fcb413fa5a5bcb331393d9d828d0cebacd1
Aliaksandr-razumovich <arazumovich@mail.ru>
2012-08-31 14:10:01 +0300

    Update diagramm/index.php

--- diagramm/index.php
+++ diagramm/index.php
@@ -5,7 +5,7 @@
    //300x300
    $im = imagecreate(300, 300);
 
-   //���������� ����� ��������
+   //Setting colors of the sectors
    $color[0] = imagecolorallocate($im, 255, 0, 0);
    $color[1] = imagecolorallocate($im, 112, 219, 147); 
    $color[2] = imagecolorallocate($im, 255, 127, 0);
@@ -14,11 +14,11 @@
    $color[5] = imagecolorallocate($im, 100, 127, 0);
    $black    = imagecolorallocate($im, 0, 0, 0);
 
-   //���������� ���� ����
+   //Setting image background color
    $back_color = imagecolorallocate($im, 255, 250, 240);
    imagefill($im, 1, 1, $back_color);
 
-   //������ ������
+   //Drawing sector
    $startAngle = 0;  
    $coordinate1 = 10;
    $coordinate2 = 15;
@@ -28,13 +28,13 @@
        $startAngle = $finishAngle;
 	   $other = 360 - $finishAngle;
 	
-	   //������� �������
+	   //Drawing the legend
 	   imagefilledrectangle($im, 10, $coordinate1 , 15, $coordinate2, $color[$k]);
 	   imagestring($im, 1, 30, $coordinate1, $name[$k].' '.round($itemPercent[$k]).'%', $black);
            $coordinate1 = $coordinate1 + 10;
 	   $coordinate2 = $coordinate2 + 10;
    }
 
-   //������� �����������
+   //Drawing the whole image
    imagepng($im);
 ?>
