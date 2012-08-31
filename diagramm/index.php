6f9fb2340d12ea6560eb75992effc9cab91c3951
Aliaksandr-razumovich <arazumovich@mail.ru>
2012-08-31 13:58:18 +0300

    Update diagramm/index.php

--- diagramm/index.php
+++ diagramm/index.php
@@ -5,7 +5,7 @@
    //300x300
    $im = imagecreate(300, 300);
 
-   //Определяем цвета секторов
+   //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
    $color[0] = imagecolorallocate($im, 255, 0, 0);
    $color[1] = imagecolorallocate($im, 112, 219, 147); 
    $color[2] = imagecolorallocate($im, 255, 127, 0);
@@ -14,27 +14,27 @@
    $color[5] = imagecolorallocate($im, 100, 127, 0);
    $black    = imagecolorallocate($im, 0, 0, 0);
 
-   //Определяем цвет фона
+   //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
    $back_color = imagecolorallocate($im, 255, 250, 240);
    imagefill($im, 1, 1, $back_color);
 
-   //Рисуем сектор
+   //пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
    $startAngle = 0;  
    $coordinate1 = 10;
    $coordinate2 = 15;
    for($k = 0; $k < count($number) - 1; $k++){
-       $finishAngle = $finishAngle + $fruitPercent[$k] * 3.6;
+       $finishAngle = $finishAngle + $itemPercent[$k] * 3.6;
        imagefilledarc($im, 150, 150, 100, 100, $startAngle, $finishAngle, $color[$k], IMG_ARC_PIE);
        $startAngle = $finishAngle;
 	   $other = 360 - $finishAngle;
 	
-	   //Выводим легенду
+	   //пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 	   imagefilledrectangle($im, 10, $coordinate1 , 15, $coordinate2, $color[$k]);
-	   imagestring($im, 1, 30, $coordinate1, $name[$k].' '.round($fruitPercent[$k]).'%', $black);
-       $coordinate1 = $coordinate1 + 10;
+	   imagestring($im, 1, 30, $coordinate1, $name[$k].' '.round($itemPercent[$k]).'%', $black);
+           $coordinate1 = $coordinate1 + 10;
 	   $coordinate2 = $coordinate2 + 10;
    }
 
-   //Выводим изображение
+   //пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
    imagepng($im);
 ?>
